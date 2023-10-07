1f268f24dd848dd8e6f53d753e45d5f5d0fca08f
ka5il <121650166+maksimmmg@users.noreply.github.com>
2023-10-07 08:39:49 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,10 @@
-
 @Echo off
 cls
 :m1
 Echo V1.3 
 Echo by ka5il
 Echo SELECT RAM:
-Echo WARNING :  The server file should be called server.jar
-Echo Only 64x system
+Echo WARNING :  The server file should be called server.jar. Only 64x system. Only Java 16.
 Echo.
 Echo 1 - 1gb
 Echo 2 - 2gb
@@ -15,9 +13,9 @@ Echo 3 - 3gb
 echo.
 Set /p choice="SELECTING RAM: "
 if not defined choice goto m1
-if "%choice%"=="1" (java -Xmx1G -Xms512M -server -XX:+UseBiasedLocking -XX:+UseG1GC -XX:+UseFastEmptyMethods -XX:ConcGCThreads=2 -jar server.jar nogui)
-if "%choice%"=="2" (java -Xmx2G -Xms512M -server -XX:+UseBiasedLocking -XX:+UseG1GC -XX:+UseFastEmptyMethods -XX:ConcGCThreads=2 -jar server.jar nogui)
-if "%choice%"=="3" (java -Xmx3G -Xms512M -server -XX:+UseBiasedLocking -XX:+UseG1GC -XX:+UseFastEmptyMethods -XX:ConcGCThreads=2 -jar server.jar nogui)
+if "%choice%"=="1" (java -Xmx1G -Xms512M -server -XX:+UseG1GC -XX:ConcGCThreads=2 -jar server.jar nogui)
+if "%choice%"=="2" (java -Xmx2G -Xms512M -server -XX:+UseG1GC -XX:ConcGCThreads=2 -jar server.jar nogui)
+if "%choice%"=="3" (java -Xmx3G -Xms512M -server -XX:+UseG1GC -XX:ConcGCThreads=2 -jar server.jar nogui)
 Echo.
 Echo PLS SELECCTING 1, 2, 3
 Echo.
